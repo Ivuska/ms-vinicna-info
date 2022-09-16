131114dbb73b0fed73f9090fadd1d90b491bbf66
Ivuska <fischerova.ivka@gmail.com>
2022-09-16 09:41:35 +0200

    Register main blueprint.

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 
+import email_addresses_bp
+
 app = Flask(__name__)
+
+app.register_blueprint(email_addresses_bp.blueprint)
 
 @app.route('/')
 def get_main_page():
