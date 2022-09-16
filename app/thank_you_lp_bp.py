86f3eb920da626e2bbac55c0224dc873f48e12ba
Ivuska <fischerova.ivka@gmail.com>
2022-09-16 23:01:07 +0200

    Deleted not necessary blueprints.

--- app/thank_you_lp_bp.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint, render_template
-blueprint = Blueprint('than_you_lp_bp', __name__)
-@blueprint.route('/thank_you')
-def show_thank_you_landing_page():
-	return render_template('thank_you_lp.html')
