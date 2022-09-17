e276c0cbeb81784dc78e8728ca111627fd207186
Ivuska <fischerova.ivka@gmail.com>
2022-09-17 10:32:24 +0200

    Added ReCaptcha.

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, flash, render_template, redirect, url_for, request
 import os
 import requests
+from flask_recaptcha import ReCaptcha
 
 
 app = Flask(__name__)
 app.secret_key = 'rbYPVS3hLzIToOJ'
+
+app.config['RECAPTCHA_SITE_KEY'] = os.environ.get('RECAPTCHA_SITE_KEY') # <-- Add your site key
+app.config['RECAPTCHA_SECRET_KEY'] = os.environ.get('RECAPTCHA_SECRET_KEY') # <-- Add your secret key
+recaptcha = ReCaptcha(app) # Create a ReCaptcha object by passing in 'app' as parameter
 
 worker_url = os.environ.get('WORKER_URL')
 
@@ -19,6 +24,10 @@ def submit_new_email():
     if r.status_code == 409:
         flash('Tenhle email už známe.')
         return redirect(url_for('get_main_page'))
+    if request.method == 'POST': # Check to see if flask.request.method is POST
+        if not recaptcha.verify(): # Use verify() method to see if ReCaptcha is filled out
+            flash('Potvrďte prosím, že nejste robot.')
+            return redirect(url_for('get_main_page'))
     return redirect(url_for('show_thank_you_landing_page'))
 
 @app.route('/thank_you')
@@ -28,3 +37,6 @@ def show_thank_you_landing_page():
 @app.route('/gdpr')
 def show_gdpr_page():
     return render_template('gdpr.html')
+
+if __name__ == '__main__':
+    app.run()
