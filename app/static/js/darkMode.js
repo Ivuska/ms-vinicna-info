e5395fdcb439cdf49f748b282d91d191bd60daa4
Ivuska <fischerova.ivka@gmail.com>
2022-09-21 00:21:00 +0200

    Dark mode, font-size, font-family, reCaptcha fix.

---
+++ app/static/js/darkMode.js
@@ -0,0 +1,24 @@
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark')
+        document.getElementById('darkmodeicon').classList.add('fa-sun')
+    } else {
+        document.documentElement.classList.remove('dark')
+        document.getElementById('darkmodeicon').classList.add('fa-moon')
+    }
+})
+  
+function setMode(){
+    if (document.documentElement.classList.contains('dark')) {
+        // Whenever the user explicitly chooses light mode
+        localStorage.theme = 'light'
+        document.documentElement.classList.remove('dark')
+        document.getElementById('darkmodeicon').classList.replace('fa-sun', 'fa-moon')
+    } else {
+        localStorage.theme = 'dark'
+        document.documentElement.classList.add('dark')
+        document.getElementById('darkmodeicon').classList.replace('fa-moon', 'fa-sun')
+
+    }
+}
