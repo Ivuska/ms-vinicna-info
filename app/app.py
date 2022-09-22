2a1361736e81a530f40d9a031c211061d587f431
Ivuska <fischerova.ivka@gmail.com>
2022-09-22 09:54:09 +0200

    CSRF token validation, deleted unnecessary icons for frontend validation.

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
-import email
 from flask import Flask, flash, render_template, redirect, url_for, request, session
+from flask_wtf.csrf import CSRFProtect, CSRFError
 from dotenv import load_dotenv
 import os
 import requests
@@ -15,6 +15,9 @@ app.templates_auto_reload = True
 app.config['RECAPTCHA_SITE_KEY'] = os.environ.get('RECAPTCHA_SITE_KEY') 
 app.config['RECAPTCHA_SECRET_KEY'] = os.environ.get('RECAPTCHA_SECRET_KEY') 
 recaptcha = ReCaptcha(app) # Create a ReCaptcha object by passing in 'app' as parameter
+
+csrf = CSRFProtect(app)
+csrf.init_app(app)
 
 worker_url = os.environ.get('WORKER_URL')
 
@@ -41,8 +44,6 @@ def submit_new_email():
         session['email_address'] = email_address
         return redirect(url_for('get_main_page'))
 
-
-
     r = requests.post(worker_url + '/email', json={ "email": email_address })
 
     if r.status_code == 409:
@@ -60,6 +61,15 @@ def show_thank_you_landing_page():
 @app.route('/gdpr')
 def show_gdpr_page():
     return render_template('gdpr.html')
+
+@app.errorhandler(CSRFError)
+def handle_csrf_error(e):
+    flash(f'{ e.description }')
+    return redirect(url_for('get_main_page'))
+
+@app.before_request
+def check_csrf():
+    csrf.protect()
 
 if __name__ == '__main__':
     app.run()
