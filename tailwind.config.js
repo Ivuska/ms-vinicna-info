3178abfaf269de46a4b884d16d2c43c1275e3876
Ivuska <fischerova.ivka@gmail.com>
2022-09-29 08:39:29 +0200

    Responsive main page- #8.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     fontFamily: {
       'sans': ['Poppins', 'sans-serif'],
     },
+    screens: {
+      'sm': '600px',
+
+      'md': '900px',
+
+      'lg': '1200px',
+
+      'xl': '1440px',
+    },
     extend: {},
   },
   darkMode: 'class',
