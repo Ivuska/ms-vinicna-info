8792e09ccfdcc457cc91c8f2e507cf1a40af750a
Ivuska <fischerova.ivka@gmail.com>
2022-10-05 09:40:34 +0200

    Get all the checking steps to one test.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,23 +1,11 @@
 describe('Got to the web app and check the main page is loaded correctly.', () => {
-  it('Visits novinky-ze-skolky.', () => {
+  it('Visits novinky-ze-skolky and check the content.', () => {
     cy.visit('https://novinky-ze-skolky.ifischerova.cz/')
-  })
-  it('Checks that switch mode icon is present.', () => {
     cy.get('[id="darkmodeicon"]')
-  })
-  it('Checks that form is present.', () => {
     cy.contains('form')
-  })
-  it('Checks that email field is present.', () => {
     cy.get('input')
-  })
-  it('Checks that recaptcha is present.', () => {
     cy.get('div[class*="recaptcha"]')
-  })
-  it('Checks that button "Chci články" is present.', () => {
     cy.get('button').contains('Chci články')
-  })
-  it('Checks that link to GDPR page is present.', () => {
     cy.contains('Informovaný souhlas')
   })
 })
