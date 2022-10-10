35ed8ef00457933d3b47c8ac23d8fa4dd3954fbf
Ivuska <fischerova.ivka@gmail.com>
2022-10-10 21:52:46 +0200

    Check sign up without confirmed recaptcha is not possible.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -9,3 +9,32 @@ describe('Got to the web app and check the main page is loaded correctly.', () =
     cy.contains('Informovaný souhlas')
   })
 })
+
+describe('Sign up without confirmed recaptcha shows error message.', () => {
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce('session')
+  })
+  it('Visits the app.', () => {
+    cy.visit('https://novinky-ze-skolky.ifischerova.cz/')
+  })
+  it('Gets an email field and type some email address in it.', () => {
+    cy.get('[id=email]')
+      .type('imatsion@seznam.cz')
+  })
+  it('Clicks on submit button.', () => {
+    cy.get('button')
+      .click()
+  })
+  it('Error flash message about missing recaptcha control is visible.', () => {
+    cy.get('[id=flashmessage]')
+      .contains('Potvrďte prosím, že nejste robot.')
+  })
+  it('Closes the error flash message.', () => {
+    cy.get('[id=closeflashmessage]')
+      .click()
+  })
+  it('Error message should not be visible.', () => {
+    cy.get('[id=flashmessage]')
+      .should('not.be.visible')
+  })
+})
