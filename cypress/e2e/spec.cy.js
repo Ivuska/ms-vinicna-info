eaa54235f89a37e5e4bb1805f0245653be2948b2
Ivuska <fischerova.ivka@gmail.com>
2022-10-22 11:07:56 +0200

    I cannot sign up for articles after the link is deactivated.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -83,7 +83,34 @@ describe('Sign up for articles.', () => {
 
     cy.get('[data-testid=input_email]').should('have.value', '')
   })
-  it.skip('I cannot sign up for articles after the link is deactivated.', () => {})
+  it('I cannot sign up for articles after the link is deactivated.', () => {
+    cy.mailtester().then(async (mailtester) => {
+      cy.log('Create and confirm email address')
+      const emailAddress = await mailtester.createAddress()
+      assert.isDefined(emailAddress)  
+
+      // sign up with inbox email address and the password
+      cy.log('Sign up the email address and verify that the email is sent.')
+      cy.get('[data-testid=input_email]').type(emailAddress);
+      cy.get('[data-testid=submit_btn]').click();
+      cy.url().should('eq', 'http://127.0.0.1:5000/thank_you').then(async () => {
+        const email = await mailtester.waitForEmail(emailAddress)
+        assert.isDefined(email);
+
+        // verify that email contains the code
+        console.log(email.body)
+        assert.strictEqual(/Klikněte pro aktivaci/.test(email.body), true)
+
+        cy.log('Visit the link with invalid token and check the flash message.')
+        const link = "http://127.0.0.1:5000/activation/d76dfb72a329"
+
+        cy.visit(link)
+        cy.get('[data-testid=flash_message]').contains('Platnost linku vypršela.')
+        cy.get('[data-testid=close_flash_message]').click()
+        cy.get('[data-testid=flash_message]').should('not.be.visible')
+      });
+    });
+  })
   it('Can generate a new email address and sign up for articles.', () => {
     cy.mailtester().then(async (mailtester) => {
       const emailAddress = await mailtester.createAddress()
@@ -126,7 +153,6 @@ describe('Sign up for articles.', () => {
     cy.log('Check the email address is successfully sent.')
     cy.url().should('eq', 'http://127.0.0.1:5000/thank_you')
     //need to set up next steps
-
   })
 });
 
@@ -159,6 +185,10 @@ describe('Unsubscribe from getting articles.', () => {
 
     cy.get('[data-testid=input_email]').should('have.value', '')
   })
-  it.skip('I cannot unsubscribe with email address that is not in signed up.', () => {})
-  it.skip('I can unsubscribe from getting articles.', () => {})
-})
+  it.skip('I cannot unsubscribe with email address that is not in signed up.', () => {
+
+  })
+  it.skip('I can unsubscribe from getting articles.', () => {
+
+  })
+});
